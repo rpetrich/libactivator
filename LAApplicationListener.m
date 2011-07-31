399d3f692e31eec0a616184a94035788766f553d
Ryan Petrich <rpetrich@gmail.com>
2011-07-31 00:18:28 -0400

    Ignore purple buddy

--- LAApplicationListener.m
+++ LAApplicationListener.m
@@ -295,7 +295,7 @@ CHConstructor {
 	if (CHLoadLateClass(SBApplicationController)) {
 		sharedApplicationListener = [[LAApplicationListener alloc] init];
 		allEventModesExceptLockScreen = [[NSArray alloc] initWithObjects:LAEventModeSpringBoard, LAEventModeApplication, nil];
-		ignoredDisplayIdentifiers = [[NSArray alloc] initWithObjects:@"com.apple.DemoApp", @"com.apple.fieldtest", @"com.apple.springboard", @"com.apple.AdSheet", @"com.apple.iphoneos.iPodOut", @"com.apple.TrustMe", @"com.apple.DataActivation", @"com.apple.WebSheet", @"com.apple.AdSheetPhone", @"com.apple.AdSheetPad", @"com.apple.iosdiagnostics", nil];
+		ignoredDisplayIdentifiers = [[NSArray alloc] initWithObjects:@"com.apple.DemoApp", @"com.apple.fieldtest", @"com.apple.springboard", @"com.apple.AdSheet", @"com.apple.iphoneos.iPodOut", @"com.apple.TrustMe", @"com.apple.DataActivation", @"com.apple.WebSheet", @"com.apple.AdSheetPhone", @"com.apple.AdSheetPad", @"com.apple.iosdiagnostics", @"com.apple.purplebuddy", nil];
 		displayStacks = (NSMutableArray *)CFArrayCreateMutable(kCFAllocatorDefault, 0, NULL);
 		CHLoadLateClass(SBApplication);
 		CHHook(8, SBApplication, initWithBundleIdentifier, webClip, path, bundle, infoDictionary, isSystemApplication, signerIdentity, provisioningProfileValidated);
