bb0f9d427d82dbaf898877fabc57e8dc920920bd
Ryan Petrich <rpetrich@gmail.com>
2011-12-25 07:06:19 -0700

    Make messagingCenter private

--- libactivator-private.h
+++ libactivator-private.h
@@ -33,6 +33,7 @@
 
 __attribute__((visibility("hidden")))
 BOOL shouldAddNowPlayingButton;
+__attribute__((visibility("hidden")))
 extern CPDistributedMessagingCenter *messagingCenter;
 
 @interface NSObject(LAListener)
